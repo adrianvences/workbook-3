abe763c406a305461270bb448eca2ff08c57f07b
adrianvences9820 <adrianvences9820@gmail.com>
2024-10-08 19:06:18 -0400

    Pushing Employee class for payroll calculator

---
+++ payroll-calculator/src/main/java/com/pluralsight/payrollCalculator/Employee.java
@@ -0,0 +1,57 @@
+package com.pluralsight.payrollCalculator;
+
+public class Employee {
+    // class properties
+    private int employeeId;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+
+    // Generated constructor
+    public Employee(int employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+
+    // Generated getters and setters
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getGrossPay() {
+        double grossPay = getPayRate() * getHoursWorked();
+        return grossPay;
+    }
+}

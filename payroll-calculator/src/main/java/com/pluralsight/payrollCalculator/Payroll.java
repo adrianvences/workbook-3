7ec4ea2b1794bb21dd9447e03de51922e17cc5f0
adrianvences9820 <adrianvences9820@gmail.com>
2024-10-09 18:05:36 -0400

    commiting before I erase extra code. this is exercise 1 for buffered writer

--- payroll-calculator/src/main/java/com/pluralsight/payrollCalculator/Payroll.java
+++ payroll-calculator/src/main/java/com/pluralsight/payrollCalculator/Payroll.java
@@ -25,27 +25,46 @@ package com.pluralsight.payrollCalculator;
 //         e.printStackTrace();
 
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Payroll {
 
     public static void main(String[] args) {
         //try block to handle exceptions will jump to catch if there is one
+        Scanner fileNameScanner = new Scanner(System.in);
+        ArrayList<Employee> employeeInfoList = new ArrayList<>();
+        System.out.print("Enter the name of the file employee file to process: ");
+        String fileToRead = fileNameScanner.nextLine();
+        System.out.print("Enter the name of the payroll file to create: ");
+        String fileToCreate = fileNameScanner.nextLine();
+
+
+
         try {
-            // created a FileReader with file
+            // created a FileReader to read file
             FileReader fileReader = new FileReader("./src/main/resources/employees.csv");
             // created buffer reader to read text from stream fileReader
             BufferedReader buffReader = new BufferedReader(fileReader);
             // Skip the header line
-            buffReader.readLine();
+//            buffReader.readLine();
+
+            // create a FileWriter
+            FileWriter fileWriter = new FileWriter(fileToCreate);
+            // create a buffered writer
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+            // header line
+            String header = buffReader.readLine();
+            String[] headerParts = header.split("\\| \n");
+            bufWriter.write(String.format(" %s | %s | %s", headerParts[0],headerParts[1],headerParts[2]));
+//            bufWriter.write(" id | name | gross pay\n");
 
             // input to assign each line from while loop
             String input;
             // as long as buffReader.readLine() does not return null each line from the file will be assigned to input
-            while((input = buffReader.readLine()) != null) {
+            while ((input = buffReader.readLine()) != null) {
                 // String array that splits each line at "|"
                 String[] employeeInfo = input.split("\\|");
 
@@ -55,22 +74,28 @@ public class Payroll {
                 double hoursWorked = Double.parseDouble(employeeInfo[2]);
                 double payRate = Double.parseDouble(employeeInfo[3]);
 
-                Employee employee = new Employee(id,name,hoursWorked,payRate);
-                System.out.printf("Employee ID: %d, Employee Name: %s, Gross pay: $%.2f \n", employee.getEmployeeId(),employee.getName(),employee.getGrossPay());
-
+                Employee employee = new Employee(id, name, hoursWorked, payRate);
+                employeeInfoList.add(employee);
+                String employeeData = String.format(" %d | %s | $%.2f \n", employee.getEmployeeId(),employee.getName(),employee.getGrossPay());
+                bufWriter.write(employeeData);
                 // prints input through each iteration of the loop
 //                System.out.println(Arrays.toString(employeeInfo));
             }
             buffReader.close();
+            bufWriter.close();
 
             //IOException just in case there is error
-        } catch(IOException e) {
+        } catch (IOException e) {
             // this provides details about the error in console if there is an error
             e.printStackTrace();
         } catch (NumberFormatException e) {
             System.err.println("error parsing number: " + e.getMessage());
+//        } finally {
+//            for (Employee employee : employeeInfoList) {
+//                System.out.printf("Employee ID: %d, Name: %s, Hours Worked: %.2f, Pay Rate: %.2f, Gross Pay: $%.2f\n",
+//                        employee.getEmployeeId(), employee.getName(), employee.getHoursWorked(), employee.getPayRate(), employee.getGrossPay());
+//            }
+
         }
-
     }
-
 }

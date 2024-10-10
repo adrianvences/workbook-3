a19fd7c9aff2bfd59f04d11fe4422bce27fb589f
adrianvences9820 <adrianvences9820@gmail.com>
2024-10-10 18:13:13 -0400

    section 3-2 Using an ArrayList Exercise 1 search inventory. Did the first bonus I replaced the code that read from array with code that reads from inventory.csv file

--- search-inventory/src/main/java/searchInventory/StoreApp.java
+++ search-inventory/src/main/java/searchInventory/StoreApp.java
@@ -1,31 +1,35 @@
 package searchInventory;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 
 public class StoreApp {
 
     public static void main(String[] args) {
-        ArrayList<Product> inventory = getInventory();
 
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("We carry the following inventory: ");
+        try {
 
-        for(int i = 0;i < inventory.size();i++) {
-            Product item = inventory.get(i);
-            System.out.printf("id: %d %s - Price: $%.2f", item.getId(),item.getName(),item.getPrice());
+            //create fileReader
+            FileReader fileReader = new FileReader("./src/main/resources/inventory.csv");
+            //bufferedReader to read from file
+            BufferedReader bufreader = new BufferedReader(fileReader);
+
+            System.out.println("We carry the following inventory: ");
+            String input;
+
+            // while loop continues until there is no next line
+            while((input = bufreader.readLine()) !=null) {
+                // each line of text is assigned to input and then printed out here.
+                System.out.println(input);
+            }
+            bufreader.close();
+
+        } catch (Exception e ) {
+            System.out.println(e.getStackTrace());
         }
     }
 
-    public static ArrayList<Product> getInventory() {
-        ArrayList<Product> inventory = new ArrayList<Product>();
-        inventory.add(new Product(1,"gameBoy",50.00f));
-        inventory.add(new Product(2,"psp",150.00f));
-        inventory.add(new Product(3,"atari",100.00f));
-        inventory.add(new Product(4,"legos", 12.50f));
-        inventory.add(new Product(5,"chicken",50.00f));
 
-        return inventory;
-    }
 }
